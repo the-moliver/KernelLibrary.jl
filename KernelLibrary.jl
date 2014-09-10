fd093fd861bf2994168a724d68bdc0e05241fee3
Michael Oliver <michael.d.oliver@gmail.com>
2014-09-09 18:54:45 -0700

    More kernels

--- KernelLibrary.jl
+++ KernelLibrary.jl
@@ -2,38 +2,35 @@ linear(x::Array, y::Vector) = (x' * y)
 
 polynomial(x::Array, y::Vector, d::Real; a::Real=1.) = (x' * y .+ a).^ d
 
-laplacian(x::Array, y::Vector, sigma::Real) = exp(-sum((x .- y).^2,1).^.5 ./ 2*sigma.^ 2.)
+laplacian(x::Array, y::Vector, sigma::Real) = exp(-sum((x.-y).^2.,1).^.5 ./ 2*sigma.^ 2.)
 
-gaussian(x::Array, y::Vector, sigma::Real) = exp(-sum((x .- y).^2,1) ./ 2*sigma.^ 2.)
+gaussian(x::Array, y::Vector, sigma::Real) = exp(-sum((x.-y).^2.,1) ./ 2*sigma.^ 2.)
 
-rbf(x::Array, y::Vector, sigma::Real) = exp(-sum(abs(x .- y),1) ./ 2*sigma.^ 2.)
+rbf(x::Array, y::Vector, sigma::Real) = exp(-sum(abs(x.-y),1) ./ 2*sigma.^ 2.)
 
+fractrbf(x::Array, y::Vector, sigma::Real, fract::Real) = exp(-sum(abs(x .- y).^fract,1).^.(1/fract) ./ 2*sigma.^ 2.)
 
-	case 'tdist'
-		dist = bsxfun(@minus, x1, x2);
-		K = (gamma((params.a+1)/2)/(sqrt(params.a*pi)*gamma(params.a/2)))*(1+(sum(dist.^2,2)/params.a)).^(-((params.a+1)/2));
+multiquad(x::Array, y::Vector, sigma::Real) = sqrt(sum((x.-y).^2.,1) .+ sigma^2.)
 
-	case 'fractrbf'
-		dist = bsxfun(@minus, x1, x2);
-		K = exp(- (sum(abs(dist).^params.d,2).^(1/params.d)) / (2*params.a^2));
-	case 'spline'
-		x12 = bsxfun(@times, x1, x2);
-	    z = 1 + x12 + (1/2)*bsxfun(@times, x12, bsxfun(@min, x1,x2)) - (1/6)*bsxfun(@min, x1, x2).^3;
-	    K = prod(z,2);
-	case 'logistic'
-		dist = bsxfun(@minus, x1, x2);
-		K = 1 ./ (1 + exp(sqrt(sum(dist.^2,2)) / params.a));
+invmultiquad(x::Array, y::Vector, sigma::Real) = 1./sqrt(sum((x.-y).^2.,1) .+ sigma^2.)
+
+function spline(x::Array, y::Vector, sigma::Real)
+	xy = x.*y
+    z = 1 .+ xy .+ (1/2).*xy.*broadcast(min, x,y) - (1/6).*broadcast(min, x,y).^3
+    vec(prod(z,1))
+end
+
+tdist(x::Array, y::Vector, a::Real) = (gamma((a+1)/2)/(sqrt(a*pi)*gamma(a/2)))*(1+(sum((x.-y).^2,1)/a)).^(-((a+1)/2))
+
+function fourier(x::Array, y::Vector, a::Real)
+    dist = x.-y;
+    dist[dist==0] = eps(eltype(x))
+    z = sin(a + 1/2)*(dist(i))./sin(dist(i)/2);
+    vec(prod(z,1))
+end
+
+logistic(x::Array, y::Vector, a::Real) = 1 ./ (1 + exp(sqrt(sum((x.-y).^2,1)) / a))
 	case 'sigmoid'
         K = tanh(params.a*x1*x2'./size(x1,2) + params.a);
-    case 'fourier'
-        z = sin(params.a + 1/2)*2*ones(size(x1,1),size(x1,2));
-        dist = bsxfun(@minus, x1, x2);
-        i = find(dist);
-        z(i) = sin(params.a + 1/2)*(dist(i))./sin(dist(i)/2);
-        K = prod(z,2);
-    case 'multiquad'
-    	dist = bsxfun(@minus, x1, x2);
-    	K = sqrt(sum(dist.^2,2) + params.a^2);
-    case 'invmultiquad'
-    	dist = bsxfun(@minus, x1, x2);
-    	K = 1./sqrt(sum(dist.^2,2) + params.a^2);
+
+
