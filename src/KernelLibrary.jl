44d0529c85fb038d6c24bcb405be438c84450147
Michael Oliver <michael.d.oliver@gmail.com>
2015-01-13 16:32:10 -0800

    remove stray period

--- src/KernelLibrary.jl
+++ src/KernelLibrary.jl
@@ -1,7 +1,7 @@
 module KernelLibrary
 
 export linear_kernel, polynomial_kernel, laplacian_kernel, gaussian_kernel, anova_kernel, rbf_kernel, fractrbf_kernel, rationalquad_kernel, multiquad_kernel, invmultiquad_kernel, circular_kernel, spherical_kernel, wave_kernel
-export power_kernel, log_kernel, spline_kernel, cauchy_kernel, chisquare_kernel, histintersect_kernel, genhistintersect_kernel, fourier_kernel, sigmoid_kernel. wavelet_kernel, transinvwavelet_kernel
+export power_kernel, log_kernel, spline_kernel, cauchy_kernel, chisquare_kernel, histintersect_kernel, genhistintersect_kernel, fourier_kernel, sigmoid_kernel, wavelet_kernel, transinvwavelet_kernel
 # Julia implementation of most of the kernel functions from
 # crsouza.blogspot.com/2010/03/kernel-functions-for-machine-learning.html
 
