46087ac076c23d75ea131f393b22498df1f41b0d
Michael Oliver <michael.d.oliver@gmail.com>
2015-01-14 18:36:31 -0800

    make functions take correct args

--- src/KernelLibrary.jl
+++ src/KernelLibrary.jl
@@ -5,99 +5,108 @@ export power_kernel, log_kernel, spline_kernel, cauchy_kernel, chisquare_kernel,
 # Julia implementation of most of the kernel functions from
 # crsouza.blogspot.com/2010/03/kernel-functions-for-machine-learning.html
 
-function linear_kernel{T}(x::Array{T}, y::Array{T}; c::Real=0.)
+function linear_kernel{T}(x::Array{T}, y::Vector{T}; c::Real=0.)
 	c=convert(T,c)
 	(y'*x .+ c)
 end
 
-function polynomial_kernel{T}(x::Array{T}, y::Array{T}, d::Real; α::Real=1., c::Real=1.)
+function polynomial_kernel{T}(x::Array{T}, y::Vector{T}, d::Real; α::Real=1., c::Real=1.)
 	d=convert(T,d); α=convert(T,α); c=convert(T,c);
 	(α.*(y'*x) .+ c).^ d
 end
 
-function laplacian_kernel{T}(x::Array{T}, y::Array{T}, σ::Real)
+function laplacian_kernel{T}(x::Array{T}, y::Vector{T}, σ::Real)
 	σ=convert(T, σ)
 	exp(-sum((x.-y).^2.,1).^.5 ./ σ)
 end
 
-function gaussian_kernel{T}(x::Array{T}, y::Array{T}, σ::Real)
+function gaussian_kernel{T}(x::Array{T}, y::Vector{T}, σ::Real)
 	σ=convert(T, σ)
 	exp(-sum((x.-y).^2,1) ./ 2*σ.^ 2)
 end
 
-function anova_kernel{T}(x::Array{T}, y::Array{T}, σ::Real, d::Real)
+function anova_kernel{T}(x::Array{T}, y::Vector{T}, σ::Real, d::Real)
 	d=convert(T,d); σ=convert(T, σ)
 	sum(exp(-σ*(x.-y).^2).^d,1)
 end
 
-rbf_kernel(x::Array, y::Array, σ::Real) = exp(-sum(abs(x.-y),1) ./ 2*σ.^ 2)
+function rbf_kernel{T}(x::Array{T}, y::Vector{T}, σ::Real)
+	σ=convert(T, σ)
+	exp(-sum(abs(x.-y),1) ./ 2*σ.^ 2)
+end
 
-fractrbf_kernel(x::Array, y::Array, σ::Real, fract::Real) = exp(-sum(abs(x .- y).^fract,1).^(1/fract) ./ 2*σ.^ 2)
+function fractrbf_kernel{T}(x::Array{T}, y::Vector{T}, σ::Real, fract::Real)
+	σ=convert(T, σ); fract=convert(T, fract); 
+	exp(-sum(abs(x .- y).^fract,1).^(1/fract) ./ 2*σ.^ 2)
+end
 
-function rationalquad_kernel{T}(x::Array{T}, y::Array{T}, c::Real)
+function rationalquad_kernel{T}(x::Array{T}, y::Vector{T}, c::Real)
 	c = convert(T, c)
 	n = sum((x.-y).^2,1)
 	1 .- n./(n .+ c)
 end
 
-multiquad_kernel(x::Array, y::Array, c::Real) = sqrt(sum((x.-y).^2.,1) .+ c^2.)
+function multiquad_kernel(x::Array, y::Vector, c::Real) = sqrt(sum((x.-y).^2.,1) .+ c^2.)
 
-invmultiquad_kernel(x::Array, y::Array, c::Real) = 1./sqrt(sum((x.-y).^2.,1) .+ c^2.)
+function invmultiquad_kernel{T}(x::Array{T}, y::Vector{T}, c::Real)
+	c = convert(T, c)
+	1 ./ sqrt(sum((x.-y).^2,1) .+ c^2)
+end
 
-function circular_kernel(x::Array, y::Array, σ::Real)
+function circular_kernel(x::Array, y::Vector, σ::Real)
 	n = sqrt(sum((x.-y).^2.,1))./σ
 	out = (2./π)*arccos(-n) - (2./π)*n*sqrt(1.0 .- n.^2)
 	out[n.>=σ] = 0
-	vec(out)
+	out
 end
 
-function spherical_kernel(x::Array, y::Array, σ::Real)
+function spherical_kernel(x::Array, y::Vector, σ::Real)
 	n = sqrt(sum((x.-y).^2.,1))./σ
 	out = 1 - 1.5.*n + .5.*n.^3
 	out[n.>=σ] = 0
-	vec(out)
+	out
 end
 
-function wave_kernel(x::Array, y::Array, θ::Real)
+function wave_kernel(x::Array, y::Vector, θ::Real)
 	n = sqrt(sum((x.-y).^2.,1))
 	vec((θ./n).*sin(n./θ))
 end
 
-power_kernel(x::Array, y::Array, d::Real) = -sqrt(sum((x.-y).^2.,1)).^d
+power_kernel(x::Array, y::Vector, d::Real) = -sqrt(sum((x.-y).^2.,1)).^d
 
-log_kernel(x::Array, y::Array, d::Real) = -log(sqrt(sum((x.-y).^2.,1)).^d + 1)
+log_kernel(x::Array, y::Vector, d::Real) = -log(sqrt(sum((x.-y).^2.,1)).^d + 1)
 
-function spline_kernel(x::Array, y::Array, σ::Real)
+function spline_kernel(x::Array, y::Vector, σ::Real)
 	xy = x.*y
 	mxy = broadcast(min, x,y)
     z = 1 .+ xy .+ xy.*mxy - ((x.+y)/2).*mxy.^2 + (1/3).*mxy.^3
     vec(prod(z,1))
 end
 
-cauchy_kernel(x::Array, y::Array, σ::Real) = 1.0 ./ (1.0 .+ (sum((x.-y).^2.,1) ./ σ.^2.))
+cauchy_kernel(x::Array, y::Vector, σ::Real) = 1.0 ./ (1.0 .+ (sum((x.-y).^2.,1) ./ σ.^2.))
 
-chisquare_kernel(x::Array, y::Array) = 1.0 .- sum((x.-y).^2 ./ (.5.*(x.+y)),1)
+chisquare_kernel(x::Array, y::Vector) = 1.0 .- sum((x.-y).^2 ./ (.5.*(x.+y)),1)
 
-histintersect_kernel(x::Array, y::Array) = sum(broadcast(min, x,y),1)
+histintersect_kernel(x::Array, y::Vector) = sum(broadcast(min, x,y),1)
 
-genhistintersect_kernel(x::Array, y::Array, α::Real, β::Real) = sum(broadcast(min, abs(x).^α,abs(y).^β),1)
+genhistintersect_kernel(x::Array, y::Vector, α::Real, β::Real) = sum(broadcast(min, abs(x).^α,abs(y).^β),1)
 
-gentstudent_kernel(x::Array, y::Array, d::Real) = 1 ./ (1.0 .+ sqrt(sum((x.-y).^2.,1)).^d)
+gentstudent_kernel(x::Array, y::Vector, d::Real) = 1 ./ (1.0 .+ sqrt(sum((x.-y).^2.,1)).^d)
 
-function fourier_kernel(x::Array, y::Array, a::Real)
+function fourier_kernel(x::Array, y::Vector, a::Real)
     dist = x.-y;
     dist[dist==0] = eps(eltype(x))
     z = sin(a + 1/2)*(dist(i))./sin(dist(i)/2);
     vec(prod(z,1))
 end
 
-sigmoid_kernel(x::Array, y::Array; α::Real=1, c::Real=0) = tanh(α*x'*y + c)
+sigmoid_kernel(x::Array, y::Vector; α::Real=1, c::Real=0) = tanh(α*x'*y + c)
 
 motherwavelet(x::Array) = cos(1.75.*x).*exp(-(x.^2)./2)
 
-wavelet_kernel(x::Array, y::Array, a::Real, c::Real) = vec(prod(motherwavelet((x.-c)./a).*motherwavelet((y.-c)./a),1))
+wavelet_kernel(x::Array, y::Vector, a::Real, c::Real) = vec(prod(motherwavelet((x.-c)./a).*motherwavelet((y.-c)./a),1))
 
-transinvwavelet_kernel(x::Array, y::Array, a::Real) = vec(prod(motherwavelet((x.-y)./a),1))
+transinvwavelet_kernel(x::Array, y::Vector, a::Real) = vec(prod(motherwavelet((x.-y)./a),1))
 
 
 
